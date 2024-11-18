33f423d84125194db64a6adb981bd3d734d7aa6c
Gangadhar Karanam <bgawai@malomatia.com>
2024-11-18 11:57:08 +0530

    testcase 2  should return the number itself for a single number

--- string-calculator-backend/StringCalculator.js
+++ string-calculator-backend/StringCalculator.js
@@ -1,6 +1,9 @@
 class StringCalculator {
     add(numbers) {
       if (numbers === '') return 0;
+      return parseInt(numbers, 10);
+
     }
+
   }
 module.exports = StringCalculator; 
--- string-calculator-backend/test.js
+++ string-calculator-backend/test.js
@@ -9,4 +9,8 @@ describe('StringCalculator', () => {
     it('should return 0 for an empty string', () => {
       expect(calculator.add("")).toBe(0);
     });
+
+    test('should return the number itself for a single number', () => {
+        expect(calculator.add("1")).toBe(1);
+      });
   });
